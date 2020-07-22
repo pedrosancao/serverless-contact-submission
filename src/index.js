1f465a9761e1fd7b065cfdc60ebb824bbc9887be
Pedro Sanção <pedrosancao@users.noreply.github.com>
2020-07-22 09:16:53 -0300

    Send e-mail with nodemailer using SMTP

---
+++ src/index.js
@@ -0,0 +1,43 @@
+const nodemailer = require('nodemailer')
+
+const app = {
+    main (req, res) {
+        app.sendMail()
+            .then(info => {
+                res.status(200).send('mail sent')
+            })
+            .catch(error => {
+                console.log(error)
+                res.status(500).send('error sending mail')
+            })
+    },
+    sendMail () {
+        return new Promise((resolve, reject) => {
+            const options = {
+                host  : process.env.SMTP_HOST,
+                name  : 'serverless',
+                secure: true,
+                auth  : {
+                    user: process.env.SMTP_USER,
+                    pass: process.env.SMTP_PASS
+                }
+            }
+            const transport = nodemailer.createTransport(options)
+            const message = {
+                from   : options.auth.user,
+                to     : 'pedrosancao@users.noreply.github.com',
+                subject: 'test contact from site',
+                text   : 'test contact from site'
+            };
+            transport.sendMail(message, function(error, info) {{}
+                if (error) {
+                    reject(error)
+                } else {
+                    resolve(info)
+                }
+            });
+        })
+    }
+}
+
+module.exports = app
